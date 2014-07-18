737811172577d226c5e53653b427e9edc2140855
Ian Simmons <isimmons33@gmail.com>
2014-07-17 23:11:42 -0400

    for the php built in server

---
+++ server.php
@@ -0,0 +1,16 @@
+<?php
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$uri = urldecode($uri);
+
+$publicPath = __DIR__.'/public'; 
+
+$requested = $publicPath.$uri;
+
+if ($uri !== '/' and file_exists($requested))
+{
+    return false;
+}
+
+require_once $publicPath.'/index.php';
