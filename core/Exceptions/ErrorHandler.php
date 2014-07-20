a4ba1666177b3d4ed9fae2c54833dd82739ddd16
Ian Simmons <isimmons33@gmail.com>
2014-07-20 12:17:32 -0400

    start the whoop whoop

---
+++ core/Exceptions/ErrorHandler.php
@@ -0,0 +1,10 @@
+<?php namespace TinyAura\Exceptions;
+
+class ErrorHandler {
+    public function load()
+    {
+        $whoops = new \Whoops\Run;
+        $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+        $whoops->register();
+    }
+}
