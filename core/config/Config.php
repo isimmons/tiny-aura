8033db389bc507e20c66989f7a7706d86af27d2f
Ian Simmons <isimmons33@gmail.com>
2014-07-20 12:19:16 -0400

    initial core config

---
+++ core/config/Config.php
@@ -0,0 +1,13 @@
+<?php namespace TinyAura\Config;
+
+class Config {
+    public function loadAll()
+    {
+        $this->configFiles = $this->getConfigFiles();
+    }
+
+    private function getConfigFiles()
+    {
+        
+    }
+}
